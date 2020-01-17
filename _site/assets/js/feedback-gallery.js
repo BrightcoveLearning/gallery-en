de2b9438732aa8855878c85064726baffae72e9b
Robert Crooks <rcrooks@brightcove.com>
2020-01-17 12:31:33 -0500

    rebuild for style fix

---
+++ _site/assets/js/feedback-gallery.js
@@ -0,0 +1,7 @@
+var fby = fby || [];
+fby.push(['showTab', {id: '15026', position: 'right', color: 'rgb(213, 37, 125)')}]);
+(function () {
+    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
+    f.src = '//cdn.feedbackify.com/f.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
+})();
