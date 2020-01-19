01aba45a4eee385599b402b71c352fcd97a133c7
Robert Crooks <rcrooks@brightcove.com>
2020-01-19 12:17:02 -0500

    rebuild site with API references, icon fixes

--- assets/js/feedback-gallery.js
+++ assets/js/feedback-gallery.js
@@ -3,7 +3,7 @@ layout: null
 ---
 
 var fby = fby || [];
-fby.push(['showTab', {id: '15026', position: 'right', color: '{{ site.product_color }}')}]);
+fby.push(['showTab', {id: '15026', position: 'right', color: '{{ site.product_color }}'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
