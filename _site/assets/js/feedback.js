48cbb34b2d38a472db1d5e8bb2c6176f74372fb7
Robert Crooks <rcrooks@brightcove.com>
2020-02-18 07:59:35 -0500

    rebuild for product logo fix

--- _site/assets/js/feedback.js
+++ _site/assets/js/feedback.js
@@ -1,5 +1,5 @@
 var fby = fby || [];
-fby.push(['showTab', {id: '', position: 'right', color: 'rgb(213, 37, 125)'}]);
+fby.push(['showTab', {id: '15026', position: 'right', color: 'rgb(213, 37, 125)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
