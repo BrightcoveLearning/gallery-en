59e0e24a5d57c18095b18fdb6c45a972d5601779
Bob Bailey <bbailey@brightcove.com>
2022-08-26 08:54:43 -0400

    add note about player analytics

--- _site/assets/s-site-assets-scripts/sajari-results-foot.js
+++ _site/assets/s-site-assets-scripts/sajari-results-foot.js
@@ -3,6 +3,7 @@
   site_domain,
   filter,
   lang_filter,
+  tab_filters,
   lang = domain.split('.')[0],
   lang_prefix = '';
 
@@ -12,10 +13,16 @@
     lang = 'en-us';
   }
   site_domain = 'gallery.support.brightcove.com';
+  console.log('site domain', site_domain);
   filter = "domain='" + site_domain +  "'";
   lang_filter = "lang='" + lang + "'";
   console.log('filter', filter);
   console.log('lang_filter', lang_filter);
+  if (site_domain === 'support.brightcove.com') {
+    tab_filters = {defaultTab:"All Brightcove Documentation",tabs:[{title:"All Brightcove Documentation",filter:lang_filter}]};
+  } else {
+    tab_filters = {defaultTab:"This Site",tabs:[{title:"This Site",filter:filter}, {title:"All Brightcove Documentation",filter:lang_filter}]};
+  }
   
   var searchInterface = sajari.init({
       mode: "inline",
@@ -23,14 +30,13 @@
       collection: "brightcove-documenation", // Set this to your collection.
       pipeline: "website",     // Set the search pipeline.
       instantPipeline: "autocomplete", // Set the instant pipeline.
-      
       attachSearchBox: document.getElementById("results-search-box"), // DOM element to render search box.
       attachSearchResponse: document.getElementById("results-search-response"), // DOM element to render search results.
       inputPlaceholder: "Search", // Placeholder text for the search box.
       inputAutoFocus: false, // Focus the input element on render.
       maxSuggestions: 5, // Maximum number of suggestions to show.
       results: {"showImages": false }, // Configure the results.
-      values: {"q.override": true, "resultsPerPage": "10","q": getUrlParam("q")}, // Set default values.
-      tabFilters: {defaultTab:"All Brightcove Documentation",tabs:[{title:"All Brightcove Documentation",filter:lang_filter}]}, // User selectable filters
+      values: {"q.override": true, "resultsPerPage": "10","q": getUrlParam("q")}, // Set default values
+      tabFilters: tab_filters, // User selectable filters
       styling: { theme: { colors: { brand: { primary: "#333" }}}}
   });
