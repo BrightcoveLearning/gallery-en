6b83f5188b3c6ff9ea8cbd53f30e19266a8a1de0
Robert <rcrooks@brightcove.com>
2022-11-20 15:51:50 -0500

    add data submodule

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -1,5 +1,7 @@
 git submodule add https://github.com/BrightcoveLearning/s-scripts
-cd _includes
+cd _data
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-data
+cd ../_includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-training
