3a58df3c8858c0d56b6b61e4d3f4b9cf7235fbd9
Robert <rcrooks@brightcove.com>
2022-12-06 18:17:45 -0500

    Nightly Build

--- _site/assets/s-site-assets-template-stylesheets/js/docs.js
+++ _site/assets/s-site-assets-template-stylesheets/js/docs.js
@@ -22,22 +22,20 @@ window.onresize=function()
 
 
 function responsiveSidebar() {
-	    console.log('smaller');
 	    sidebar.classList.remove('sidebar-visible');
 		sidebar.classList.add('sidebar-hidden');
-    let w = window.innerWidth;
-	// if(w >= 1200) {
-	//     // if larger 
-	//     console.log('larger');
-	// 	sidebar.classList.remove('sidebar-hidden');
-	// 	sidebar.classList.add('sidebar-visible');
-		
-	// } else {
-	//     // if smaller
-	//     console.log('smaller');
-	//     sidebar.classList.remove('sidebar-visible');
-	// 	sidebar.classList.add('sidebar-hidden');
-	// }
+   		let w = window.innerWidth;
+		let url = window.location.href;
+		if(w >= 1200 && url.includes('.html')) {
+			sidebar.classList.remove('sidebar-hidden');
+			sidebar.classList.add('sidebar-visible');
+			
+		} else {
+			
+			console.log('smaller');
+			sidebar.classList.remove('sidebar-visible');
+			sidebar.classList.add('sidebar-hidden');
+		}
 };
 
 sidebarToggler.addEventListener('click', () => {
