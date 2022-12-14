fdc93af90237e77019ffce516b83947dc514ec20
Robert <rcrooks@brightcove.com>
2022-12-13 20:46:03 -0500

    Nightly Build

--- _site/s-scripts/push-all-quiet.sh
+++ _site/s-scripts/push-all-quiet.sh
@@ -2,18 +2,18 @@ echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
 read COMMITMESSAGE 
 cd ..
 git pull --quiet
-bundle update
-bundle exec jekyll build --trace --incremental
+bundle update > /dev/null
+bundle exec jekyll build --trace --incremental > /dev/null
 setopt localoptions rmstarsilent
 rm -f _site/feed.xml
 bold=`tput bold`
 normal=`tput sgr0`
 git pull --quiet
-git add .
+git add . > /dev/null
 git commit -m $COMMITMESSAGE --quiet
 git push --quiet
 git submodule --quiet foreach "git fetch --quiet origin main"
-git submodule --quiet foreach "git checkout --quiet main ; git pull --quiet; git add . ; git commit -m '$COMMITMESSAGE'  --quiet; git push --quiet"
-git add .
-git commit -m$COMMITMESSAGE  --quiet 
+git submodule --quiet foreach "git checkout --quiet main ; git pull --quiet; git add . > /dev/null; git commit -m '$COMMITMESSAGE'  --quiet; git push --quiet"
+git add . > /dev/null
+git commit -m '$COMMITMESSAGE' --quiet 
 git push --quiet
