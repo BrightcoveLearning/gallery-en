53295b3b85d93449f12ce56fce078aa4af167b61
Bob Bailey <bbailey@brightcove.com>
2023-01-24 11:05:43 -0500

    fix links

--- _site/s-scripts/pull-all-v2.sh
+++ _site/s-scripts/pull-all-v2.sh
@@ -3,5 +3,5 @@ cd ..
 git submodule update --remote
 git submodule foreach git checkout main
 git submodule foreach git pull origin main 
-git commit -a -m "Commit in pull all"
+# git commit -a -m "Commit in pull all"
 git pull
