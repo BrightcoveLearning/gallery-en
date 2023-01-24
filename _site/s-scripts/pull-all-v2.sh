fba483e50cd0c2a6b79893db4a9a8e9cb1e88f7b
Robert <rcrooks@brightcove.com>
2023-01-23 19:09:04 -0500

    Nightly Build

--- _site/s-scripts/pull-all-v2.sh
+++ _site/s-scripts/pull-all-v2.sh
@@ -1,5 +1,6 @@
 cd ..
-git pull
-git submodule update 
+git submodule update --remote
 git submodule foreach git checkout main
 git submodule foreach git pull origin main 
+git commit -am "Commit in pull all"
+git pull
