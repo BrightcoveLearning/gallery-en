24fafb1f92b6990cc1335d217626208f86a979a2
Yarianna Tineo <47702016+yariannatineo@users.noreply.github.com>
2023-01-24 11:44:17 -0600

    Index page links fixed

--- _site/s-scripts/pull-all-v2.sh
+++ _site/s-scripts/pull-all-v2.sh
@@ -1,6 +1,7 @@
+# Testing new pull all
 cd ..
 git submodule update --remote
 git submodule foreach git checkout main
 git submodule foreach git pull origin main 
-git commit -am "Commit in pull all"
+# git commit -a -m "Commit in pull all"
 git pull
