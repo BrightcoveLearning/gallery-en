009aba1dfe2880803f22fed54b3f7ce6dd3c14df
Robert <rcrooks@brightcove.com>
2023-02-05 20:36:08 -0500

    Nightly Build

--- _site/assets/s-site-assets-scripts/sajari-search-box.js
+++ _site/assets/s-site-assets-scripts/sajari-search-box.js
@@ -3,7 +3,7 @@ var searchBox = sajari.init({
     project: "1588255093746585379", // Set this to your project.
     collection: "brightcove-documentation", // Set this to your collection.
     instantPipeline: "autocomplete", // Pipeline used as you type
-    inputPlaceholder: "Search in the docs...", // Input element placeholder
+    inputPlaceholder: "Search", // Input element placeholder
     maxSuggestions: 5, // Maximum number of suggestions to show
     attachSearchBox: document.getElementById("nav-search-box") // DOM element to attach to
   });
