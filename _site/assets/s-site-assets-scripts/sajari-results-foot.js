bf16865858f5f996ad7410c21cfba4b1a3a41dcc
Robert <rcrooks@brightcove.com>
2023-02-25 09:44:01 -0500

    Nightly Build

--- _site/assets/s-site-assets-scripts/sajari-results-foot.js
+++ _site/assets/s-site-assets-scripts/sajari-results-foot.js
@@ -5,7 +5,9 @@
   lang_filter,
   tab_filters,
   lang = domain.split('.')[0],
-  lang_prefix = '';
+    lang_prefix = '',
+    this_site_filter = '',
+    all_bc_docs_filter = '';
 
   if (lang === 'ja' || lang === 'ko' || lang === 'es' || lang === 'fr' || lang === 'de' || lang === 'zh-tw') {
     lang_prefix = lang + '.';
@@ -15,15 +17,30 @@
   site_domain = 'gallery.support.brightcove.com';
   console.log('site domain', site_domain);
   filter = "domain='" + site_domain +  "'";
-  lang_filter = "lang='" + lang + "'";
-  console.log('filter', filter);
-  console.log('lang_filter', lang_filter);
-  if (site_domain === 'support.brightcove.com') {
-    tab_filters = {defaultTab:"All Brightcove Documentation",tabs:[{title:"All Brightcove Documentation",filter:filter}]};
+lang_filter = "lang='" + lang + "'";
+  
+  //Build filters based on if foreign language site or English
+  if (lang === 'ja' || lang === 'ko' || lang === 'es' || lang === 'fr' || lang === 'de' || lang === 'zh-tw') {
+    all_bc_docs_filter = "domain  ~ '" + lang + ".'";
+    this_site_filter = "domain  ~ '" + site_domain + "'";
   } else {
-    tab_filters = {defaultTab:"This Site",tabs:[{title:"This Site",filter:filter}, {title:"All Brightcove Documentation",filter:lang_filter}]};
+    all_bc_docs_filter = "domain  ^ 'studio.support.' OR domain  ^ 'mstudio.support.' OR domain  ^ 'player.support.' OR domain  ^ 'apis.support.' OR domain  ^ 'sdks.support.' OR domain  ^ 'integrations.support.' OR domain  ^ 'general.support.' OR domain  ^ 'audience-insights.support.' OR domain  ^ 'beacon-help.support.' OR domain  ^ 'beacon.support.' OR domain  ^ 'corptv.support.' OR domain  ^ 'live.support.'  OR domain  ^ 'zencoder.support.'  OR domain  ^ 'gallery.support.' OR domain  ^ 'interactivity.support.' OR domain  ^ 'ssai.support.' OR domain  ^ 'campaign.support.'";
+    this_site_filter = "domain  ~ '" + site_domain + "'"
   }
   
+  console.log('filter', filter);
+  console.log('lang_filter', lang_filter);
+  console.log('this_site_filter: ', this_site_filter);
+  console.log('all_bc_docs_filter: ', all_bc_docs_filter);
+  
+  // Apply the filters if it is Support home or one of the product domains
+  if (site_domain === 'support.brightcove.com') {
+    tab_filters = {defaultTab:"All Brightcove Documentation",tabs:[{title:"All Brightcove Documentation",filter:all_bc_docs_filter}]};
+  } else {
+    tab_filters = {defaultTab:"This Site",tabs:[{title:"This Site",filter:this_site_filter}, {title:"All Brightcove Documentation",filter:all_bc_docs_filter}]};
+  }
+  
+
   var searchInterface = sajari.init({
       mode: "inline",
       project: "1588255093746585379", // Set this to your project.
