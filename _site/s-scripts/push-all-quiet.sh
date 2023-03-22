c5df8b6a21c25046b8187d0219b26fc93273b59f
Robert <rcrooks@brightcove.com>
2023-03-22 18:45:21 -0400

    Nightly Build

--- _site/s-scripts/push-all-quiet.sh
+++ _site/s-scripts/push-all-quiet.sh
@@ -1,3 +1,4 @@
+# Push all with quiet statements
 echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
 read COMMITMESSAGE 
 cd ..
@@ -14,16 +15,16 @@ echo 'Added changed files in parent repo'
 git pull --quiet
 git add . > /dev/null
 echo 'Committing files in parent repo'
-git commit -m '$COMMITMESSAGE' --quiet
+git commit -m $COMMITMESSAGE --quiet
 echo 'Pushing files in parent repo'
 git push --quiet
 echo 'Getting all submodules'
 git submodule --quiet foreach "git fetch origin main --quiet"
 echo 'Writing to submouldes'
-git submodule --quiet foreach "git checkout --quiet main  ; echo 'a' ; git pull --quiet; echo 'b' ;  git add . > /dev/null; echo 'c' ; git commit  --quiet -m '$COMMITMESSAGE'; echo 'd' ;  git push --quiet" --quiet
+git submodule --quiet foreach "git checkout --quiet main ; git pull --quiet; git add . > /dev/null; git commit  --quiet -m '$COMMITMESSAGE';  git push --quiet"
 echo 'Syncing changed submodules with parent repo'
 git add . > /dev/null
 echo 'Committing changes'
-git commit -m '$COMMITMESSAGE' --quiet
+git commit -m $COMMITMESSAGE --quiet
 echo 'Pushing parent repo changes'
 git push --quiet
